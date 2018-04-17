e0c13c0d787e33f91c7d985aa7e5e14ddb966c14
Graeme Smith <graeme_c_smith@hotmail.com>
2018-04-17 10:55:58 +0100

    Skeleton test class added

---
+++ tests/basic_tests.py
@@ -0,0 +1,19 @@
+"""Contains tests for array_spiker.py and associate scripts"""
+
+import unittest2 as unittest
+from helpers import *
+
+
+class CurvilinearTest(unittest.TestCase):
+    """Tests for curvilinearQC.py."""
+
+    def test_XXXX(self):
+        """Is blah blah"""
+
+        self.assertEquals(1,
+                          1,
+                          msg="TODO:")
+
+
+if __name__ == '__main__':
+    unittest.main()
