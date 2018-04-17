e4cb053946e815b50a72f29140f23ab43aeea57e
Graeme Smith <graeme_c_smith@hotmail.com>
2018-04-17 12:04:51 +0100

    Added test_pixel_intensities()

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -7,11 +7,15 @@ from helpers import *
 class CurvilinearTest(unittest.TestCase):
     """Tests for curvilinearQC.py."""
 
-    def test_XXXX(self):
-        """Is blah blah"""
-
-        self.assertEquals(1,
-                          1,
+    def test_pixel_intensities(self):
+        """Test that pixel_intensities() returns correct output"""
+        test_array = np.array([[0, 0, 0], [1, 1, 1], [1, 2, 3]])
+        col_mean, row_mean = pixel_intensities(test_array)
+        self.assertEquals(cols,
+                          [0.66666667, 1., 1.33333333],
+                          msg="TODO:")
+        self.assertEquals(row_mean,
+                          [0., 1., 2.],
                           msg="TODO:")
 
 
