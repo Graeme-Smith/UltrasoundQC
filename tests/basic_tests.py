126bb3e85a10ee0e8b62461950e3128e3993fc5d
Graeme Smith <graeme_c_smith@hotmail.com>
2018-04-17 12:34:04 +0100

    Debugged test_pixel_intensities()

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,4 +1,4 @@
-"""Contains tests for array_spiker.py and associate scripts"""
+"""Contains tests for curvilinearQC.py and associate scripts"""
 
 import unittest2 as unittest
 from helpers import *
@@ -11,12 +11,13 @@ class CurvilinearTest(unittest.TestCase):
         """Test that pixel_intensities() returns correct output"""
         test_array = np.array([[0, 0, 0], [1, 1, 1], [1, 2, 3]])
         col_mean, row_mean = pixel_intensities(test_array)
-        self.assertEquals(cols,
-                          [0.66666667, 1., 1.33333333],
-                          msg="TODO:")
-        self.assertEquals(row_mean,
-                          [0., 1., 2.],
-                          msg="TODO:")
+        message = "pixel_intensities() returning incorrect values"
+        self.assertAlmostEqual(col_mean[0], 0.66666667, msg=message)
+        self.assertAlmostEqual(col_mean[1], 1., msg=message)
+        self.assertAlmostEqual(col_mean[2], 1.33333333, msg=message)
+        self.assertAlmostEqual(row_mean[0], 0., msg=message)
+        self.assertAlmostEqual(row_mean[1], 1., msg=message)
+        self.assertAlmostEqual(row_mean[2], 2., msg=message)
 
 
 if __name__ == '__main__':
