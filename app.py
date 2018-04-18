16581c03cbcfca901d2eb536f04504e7d0836b9d
Graeme Smith <graeme_c_smith@hotmail.com>
2018-04-18 12:40:02 +0100

    Dropdown populated with file names

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from helpers import *
+
+# Import selected files. TODO Remove hard coded reference to import directory.
+files = import_batch_images("/home/graeme/PycharmProjects/UltrasoundQC/test_data/")
 
 app = dash.Dash()
 app.layout = html.Div([
+    html.H1(children='Ultrasound Reverb Analysis'),
     dcc.Dropdown(
         id='file-dropdown',
-        options=[],
+        options=[{'label': i, 'value': i} for i in files],  # Populate drop down with file names
         value=None
     ),
     html.Div(id='output-container')
