b7f1bcbbd1a157c66fd729f4526fc67396b4fe3d
Graeme Smith <graeme_c_smith@hotmail.com>
2018-05-01 10:24:19 +0100

    Added tabs to Dash display+image selection

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import base64  # pybase64 required to serve static files to dash server.
 import dash
+from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
 import flask
@@ -13,11 +14,13 @@ from helpers  import *
 # Import selected files. TODO Remove hard coded reference to import directory.
 image_path = "/home/graeme/PycharmProjects/UltrasoundQC/test_data/"
 image_files = import_batch_images(image_path)
-static_image_route = '/static'  # Required to serve local images to Flask server
+user_selected_file = None
+
 
 app = dash.Dash(__name__, static_folder='assets')
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
+app.config['suppress_callback_exceptions']=True
 
 colors = {
     'background': '#111111',
@@ -39,136 +42,45 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},
                               # Populate drop down with file names
                               value=None
                           ),
-
-                          html.Img(id='loaded_image'),
-                          html.Div(id='output-container'),
-                          html.Div(
-                              [
-                                  html.Div(
-                                      [
-                                          dcc.Graph(id='main_graph')
-                                      ],
-                                      className='eight columns',
-                                      style={'margin-top': '20'}
-                                  ),
-                                  html.Div(
-                                      [
-                                          dcc.Graph(id='vertical_graph')
-                                      ],
-                                      className='four columns',
-                                      style={'margin-top': '20'}
-                                  ),
+                          dcc.Tabs(
+                              tabs=[
+                                  {'label': 'Select Image', 'value': 1},
+                                  {'label': 'Image Analysis', 'value': 2},
+                                  {'label': '3d Intensity Plot', 'value': 3}
                               ],
-                              className='row'),
-                          html.Div(
-                              [
-                                  html.Div(
-                                      [
-                                          dcc.Graph(id='horizontal_graph')
-                                      ],
-                                      className='eight columns',
-                                      style={'margin-top': '20'}
-                                  ),
-                                  html.Div(
-                                      [
-                                          dcc.Graph(id='thumb_nail')
-                                      ],
-                                      className='four columns',
-                                      style={'margin-top': '20'}
-                                  ),
-                              ],
-                              className='row'),
-                          html.Div(
-                              [
-                                  html.Div(
-                                      [
-                                          dcc.Graph(id='3d_graph')
-                                      ],
-                                      className='eight columns',
-                                      style={'margin-top': '20'}
-                                  ),
-                                  html.Div(
-                                      [
-                                          dcc.Graph(id='test_graph3')
-                                      ],
-                                      className='four columns',
-                                      style={'margin-top': '20'}
-                                  ),
-                              ],
-                              className='row')
-
+                              value=1,
+                              id='tabs',
+                              vertical=False
+                          ),
+                          html.Div([], id='tab-output')
                       ])
 
 
-@app.callback(
-    dash.dependencies.Output('output-container', 'children'),
-    [dash.dependencies.Input('file-dropdown', 'value')])
-def update_output(value):
-    if value is not None:
-        img = cv2.imread(value)
-        print img
-    return 'You have selected "{}"'.format(value)
-
-
-@app.callback(
-    dash.dependencies.Output('main_graph', 'figure'),
-    [dash.dependencies.Input('file-dropdown', 'value')])
-def update_plots(value):
-    if value is not None:
-        img, grey_image, blurred_image, threshold_image = import_image(value)
-        ultrasound_cnt, cnt, convex, corners = detect_reverb(threshold_image)
-        crop_img = mask_background(img, ultrasound_cnt)
-        # Convert from numpy array to image format:
-        crop_img = Image.fromarray(crop_img)
-        horizontal_intensity = np.mean(crop_img, axis=0)  # Average by column
-        vertical_intensity = np.mean(crop_img, axis=1)  # Average by row
-
-        horiz_trace = go.Scatter(
-            x=range(0, len(horizontal_intensity), 1),
-            y=horizontal_intensity,
-            mode='lines',
-            name='lines'
-        )
-
-        int_data = [horiz_trace]
-        layout = go.Layout(
-            images=[dict(
-                source=value,
-                xref="x",
-                yref="y",
-                x=100,
-                y=60,
-                sizex=100,
-                sizey=100,
-                opacity=1,
-                layer="below")],
-            xaxis=dict(
-                domain=[0, 0.45]
-            ),
-            yaxis=dict(
-                domain=[0, 0.45]
-            )
-        )
-        # fig = go.Figure(data=int_data, layout=layout)
-        # py.plot(fig, filename='horizontal.html')
-        my_figure = dict(data=int_data, layout=layout)
-        return my_figure
-    else:
-        pass
-
-
-@app.callback(
-    dash.dependencies.Output('loaded_image', 'src'),
-    [dash.dependencies.Input('file-dropdown', 'value')])
-def update_image_src(value):
-    encoded_image = base64.b64encode(open(value, 'rb').read())
-    #app.layout = html.Div([
-        #html.Img(src='data:image/png;base64,{}'.format(encoded_image))
-    selected_image = 'data:image/png;base64,{}'.format(encoded_image)
-    return selected_image
-
+@app.callback(Output('tab-output', 'children'),
+              [Input('tabs', 'value'), Input('file-dropdown', 'value')])
+def display_content(open_tab, file_name):
+    # Selected Image Tab
+    if open_tab == 1:
+        if file_name is None:
+            pass
+        else:
+            encoded_image = base64.b64encode(open(file_name, 'rb').read())
+            selected_image = 'data:image/png;base64,{}'.format(encoded_image)
+            return html.Img(id='loaded_image', src=selected_image)
+    # Image Analysis Tab
+    if open_tab == 2:
+        if file_name is None:
+            pass
+        else:
+            pass
+    # 3d Intensity Tab
+    if open_tab == 3:
+        if file_name is None:
+            pass
+        else:
+            encoded_image = base64.b64encode(open(file_name, 'rb').read())
+            pass
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
+    app.run_server(debug=True,
+                   port=8045)
